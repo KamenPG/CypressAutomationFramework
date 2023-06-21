c82124ed8ef13c8233faee553d959c3783b496cf
kamen.georgiev <kamen971@gmail.com>
2023-06-21 14:44:10 +0300

    adding page object design and parameterization

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,14 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  defaultCommandTimeout: 6000,
+  env: {
+    url: "https://rahulshettyacademy.com/angularpractice"
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    specPattern: 'cypress/e2e/tests/*.js'
+    specPattern: 'cypress/e2e/tests/*.cy.js'
   },
 });
--- cypress/e2e/tests/spec.cy.js
+++ cypress/e2e/tests/spec.cy.js
@@ -12,7 +12,7 @@ this.data = data
   it('My first test', function() {
     const homePage = new HomePage()
     const productPage = new ProductPage()
-    cy.visit('https://rahulshettyacademy.com/angularpractice')
+    cy.visit(Cypress.env('url'))
 
     homePage.getEditBox().type(this.data.name)
     homePage.getGender().select(this.data.gender)
@@ -27,6 +27,39 @@ this.data = data
       cy.addProduct(element)
     })
 
+    Cypress.config('defaultCommandTimeout', 10000)
+
     productPage.checkOutButton().click()
+    var sum = 0
+
+    cy.get('tr td:nth-child(4) strong').each(($el, index, $list) => {
+      
+      const amount = $el.text()
+      var res = amount.split(" ")
+      res = res[1].trim()
+      sum = Number(sum) + Number(res)
+    
+    })
+
+    cy.get('h3 > strong').then(function($element)
+    {
+      const totalAmount = $element.text()
+      var resTotalAmount = totalAmount.split(" ")
+      resTotalAmount = Number(resTotalAmount[1].trim())
+      expect(sum).to.equal(resTotalAmount)
+    })
+   
+    cy.contains('Checkout').click()
+    cy.get('#country').type('Germany')
+    cy.get('.suggestions > ul > li > a').trigger('mouseover').click()
+    cy.get('label[for="checkbox2"]').click()
+    cy.get('input[type="submit"]').click()
+    cy.get('.alert').then(function(element)
+    {
+      const actualText = element.text()
+      expect(actualText.includes('Success')).to.be.true
+
+    })
+
   })
 })
