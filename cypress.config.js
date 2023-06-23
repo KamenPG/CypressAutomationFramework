0416088d0928e75d57cbfdc754264193c341f907
kamen.georgiev <kamen971@gmail.com>
2023-06-23 11:20:47 +0300

    adding scripts and jenkins integration

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "nxtfec",
   defaultCommandTimeout: 6000,
   env: {
     url: "https://rahulshettyacademy.com/angularpractice"
